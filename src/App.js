eb0b25570e8033cb2e01b2ebea597dfd6b4746b3
thanmayatanu1 <thanmayatanu1@gmail.com>
2023-06-14 13:17:45 +0530

    react project

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import ExpenseItem from './components/ExpenseItem'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,9 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>ExpenseItems</h2>
+        <ExpenseItem></ExpenseItem>
         <a
           className="App-link"
           href="https://reactjs.org"
