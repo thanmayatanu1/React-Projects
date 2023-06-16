bd25a602f76f290b91719ee5d58f0bb5962b568d
thanmayatanu1 <thanmayatanu1@gmail.com>
2023-06-16 16:45:13 +0530

    correct props commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,26 +1,19 @@
 import './ExpenseItem.css';
-function ExpenseItem(props)
-{
-    
-
-    const locationOfExpenditure = new Date(2021, 2, 28);
-    const locationOfExpenditureTitle = 'Car Insurance';
-    const locationOfExpenditureAmount = 294.67;
+const ExpenseItem = (props) => {
     return (
-        <div className='expense-item'>
-            <div>
-                {locationOfExpenditure.toISOString()}
-            </div>
-            <div className='expense-item_description'>
-                <h2>{locationOfExpenditureTitle}</h2>
-            </div>
-            <div className='expense-item_price'>
-               ${locationOfExpenditureAmount}
-            </div>
+      <div className='expense-item'>
+        <div>
+        {props.date.toISOString()}
         </div>
-
-
-
+        
+        <div className='expense-item_description'>
+            <h2>{props.title}</h2>
+          </div>
+          <div className='expense-item_price'>${props.amount}</div>
+        </div>
+      
     );
-}
-export default ExpenseItem;
+  };
+  
+  export default ExpenseItem;
+  
