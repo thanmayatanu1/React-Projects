3d96abdd65bb98506525cec461ce0acd0cc21375
thanmayatanu1 <thanmayatanu1@gmail.com>
2023-06-20 12:00:32 +0530

    Advance react dom manipulation add expense

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,32 @@
+import React, {useState} from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./UI/Card";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
+  
   const clickHandler = () => {
-    console.log("Clicked!!!!");
+    setTitle("Updated!!!");
+   
+    console.log(title);
+  }
+
+  const AddExpense = () => {
+    setAmount("100$");
+   
+    console.log(amount);
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
       <button onClick = {clickHandler}>Delete Expense</button>
+      <button onClick = {AddExpense}>Add Expense Expense</button>
     </Card>
   );
 };
